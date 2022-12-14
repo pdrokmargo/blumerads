1f8d067b9fea45338cfb88b7c2df03398eb381fa
Evianzx <eduardo.caldn2@gmail.com>
2022-12-14 12:02:56 -0500

    Genericbotton
    boton genrico para usar con
    purple'
    'confirmation'
    'cancel'
    'alternative'
    'auxiliar'

--- src/app/common/components/purple-button/purple-button.component.ts
+++ src/app/common/components/purple-button/purple-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Button } from 'bootstrap';
 
 @Component({
@@ -6,16 +6,14 @@ import { Button } from 'bootstrap';
   templateUrl: './purple-button.component.html',
   styleUrls: ['./pruple-button.component.scss']
 })
-export class PurpleButtonComponent implements OnInit {
+export class PurpleButtonComponent {
 
   @Input() textDescription: String | undefined = 'Crear campaña guiada' ;
+  @Output() ClickAddPurplebutton = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit(): void {
+ 
+  Onclick(){
+    this.ClickAddPurplebutton.emit();
   }
-  
-	ClickAddPurplebutton() {
-		
-  }
+	
 }

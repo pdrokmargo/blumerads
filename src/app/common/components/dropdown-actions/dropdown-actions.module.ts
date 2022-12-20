63598d3b736b6940ae18c9e96e9d2f78c68e4f82
dhenriquez5 <dhenriquezturcios@gmail.com>
2022-12-20 15:42:50 -0500

    Create Component

---
+++ src/app/common/components/dropdown-actions/dropdown-actions.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DropdownActionsComponent } from './dropdown-actions.component';
+
+
+
+@NgModule({
+  declarations: [
+    DropdownActionsComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    DropdownActionsComponent
+  ]
+})
+export class DropdownActionsModule { }

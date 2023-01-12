5e5e4431cb1b2cc0df146a04f34d5ece1112fe32
Mijail Gonzalez <mrgonzalez@gbm.net>
2023-01-12 18:34:25 -0500

    se crea modulo de material para enlazar componentes relacionados con material e independizar de los componentes nativos

---
+++ src/app/modules/material/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatChipsModule} from "@angular/material/chips";
+
+
+
+@NgModule({
+  declarations: [
+    // tu componente de material
+  ],
+  imports: [
+    CommonModule,
+    MatChipsModule
+  ]
+})
+export class MaterialModule { }

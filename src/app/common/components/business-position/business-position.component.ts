47354ba8709c9220d3731ae446bce173cb387c00
Mijail Gonzalez <mrgonzalez@gbm.net>
2023-01-30 23:27:46 -0500

    se ajusta el titulo del label business position

--- src/app/common/components/business-position/business-position.component.ts
+++ src/app/common/components/business-position/business-position.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input, Output , EventEmitter } from '@angular/core';
 export class BusinessPositionComponent implements OnInit {
   selConfig: DropDownActionI[];
   selectedOption = 1;
-  label: String = 'Cargo empresarial';
+  @Input() title: String = '';
 
 
   @Input() selectConfig: DropDownActionI[];

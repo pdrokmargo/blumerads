c631c10186aa82975c3fde5eff6b7d876cc95359
Weinner David <weinnerdavid@gmail.com>
2023-05-02 23:22:08 -0500

    Stepper and avatar

--- src/app/common/components/avatarprofile/avatarprofile.component.ts
+++ src/app/common/components/avatarprofile/avatarprofile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit , Input} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-avatarprofile',
@@ -6,13 +6,46 @@ import { Component, OnInit , Input} from '@angular/core';
   styleUrls: ['./avatarprofile.component.scss']
 })
 export class AvatarprofileComponent implements OnInit {
-
-  @Input() name: String = 'Carolina Fernández' ;
-  @Input() id: number | string = "ID:15241dafasa-asffds-sdfasfsdf";
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    const avatarImage = document.querySelector('.avatar-image img') as HTMLImageElement;
+    const editIcon = document.querySelector('.edit-icon');
+    const deleteIcon = document.querySelector('.delete-icon');
+    let fileInput = document.querySelector('.file-input') as HTMLInputElement;
 
+    if (editIcon) {
+      editIcon.addEventListener('click', () => {
+        // Código para cambiar la imagen
+        fileInput.addEventListener('change', (event: Event) => {
+          const file = (event.target as HTMLInputElement).files?.[0];
+          if (file) {
+            const reader = new FileReader();
+            reader.onload = () => {
+              avatarImage.src = reader.result as string;
+            };
+            reader.readAsDataURL(file);
+          }
+        });
+        fileInput.click();
+      });
+    }
+
+    if (deleteIcon) {
+      deleteIcon.addEventListener('click', () => {
+        const confirmation = confirm('¿Estás seguro de que deseas eliminar este avatar?');
+        if (confirmation) {
+          const avatarContainer = document.querySelector('.avatar-image');
+          if (avatarContainer) {
+            avatarContainer.remove();
+            const newFileInput = document.createElement('input');
+            newFileInput.classList.add('file-input');
+            newFileInput.type = 'file';
+            avatarContainer.appendChild(newFileInput);
+            fileInput = newFileInput;
+          }
+        }
+      });
+    }
+  }
 }
